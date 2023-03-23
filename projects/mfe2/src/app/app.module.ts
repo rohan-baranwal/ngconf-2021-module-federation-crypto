b988afe75ea81b391cd98026d4eaa2b882c0f3bf
Rohan Baranwal <baranwal.rohan0@gmail.com>
2023-03-23 23:09:27 +0530

    added new component to mfe 2

--- projects/mfe2/src/app/app.module.ts
+++ projects/mfe2/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Chart1Component } from './chart1/chart1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chart1Component
   ],
   imports: [
     BrowserModule
